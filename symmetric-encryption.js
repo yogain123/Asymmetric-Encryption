60b208379b94c6f2d8591849ebce899cdd25552a
yogain123 <yogendrasaxena56@gmail.com>
2019-12-23 17:22:53 +0530

    few methods checking in encode-decode

--- symmetric-encryption.js
+++ symmetric-encryption.js
@@ -6,6 +6,10 @@ let text = "My name is yogendra Saxena";
 // Creating Random key for Symmetric Encryption
 let key = crypto.randomBytes(16);
 console.log({key});
+const utfString = key.toString("utf8");
+const hex = key.toString("hex");
+const base64 = key.toString("base64");
+console.log({utfString, hex, base64})
 let encrypt = crypto.createCipher("aes-256-ctr",key).update(text, 'utf-8','hex');
 console.log({encrypt});
 let decrypt = crypto.createDecipher("aes-256-ctr",key).update(encrypt,'hex', 'utf-8');
