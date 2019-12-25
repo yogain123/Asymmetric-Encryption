6b507a380e7ff9d1440784f855ae18ced2d21a0e
yogendra saxena <yogendrasaxena56@gmail.com>
2019-12-25 15:32:39 +0530

    Update symmetric-encryption.js

--- symmetric-encryption.js
+++ symmetric-encryption.js
@@ -10,8 +10,8 @@ const utfString = key.toString("utf8");
 const hex = key.toString("hex");
 const base64 = key.toString("base64");
 console.log({utfString, hex, base64})
-let encrypt = crypto.createCipher("aes-256-ctr",key).update(text, 'utf-8','hex');
+let encrypt = crypto.createCipher("aes-256-ctr",utfString).update(text, 'utf-8','hex');
 console.log({encrypt});
-let decrypt = crypto.createDecipher("aes-256-ctr",key).update(encrypt,'hex', 'utf-8');
+let decrypt = crypto.createDecipher("aes-256-ctr",utfString).update(encrypt,'hex', 'utf-8');
 console.log({decrypt});
 })()
